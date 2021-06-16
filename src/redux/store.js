e501bace125c65e125bbc8b17968c3626240ae8e
Rahul Rao <rahulrao@email.com>
2021-06-16 12:29:10 +0800

    added swipe up handler to home page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import mediaReducer from './reducers/media';
+import homeReducer from './reducers/home';
 
-const rootReducer = combineReducers({ mediaReducer });
+const rootReducer = combineReducers({ mediaReducer, homeReducer });
 
 export const Store = createStore(rootReducer, applyMiddleware(thunk));
