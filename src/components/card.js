b4d2f65d736b8a2929a29c5a4f037328f9542664
Rahul Rao <rahulrao@email.com>
2021-06-17 11:40:24 +0800

    text margins changed, new card styles, updated margins

--- src/components/card.js
+++ src/components/card.js
@@ -7,7 +7,7 @@ import Scrubber from './sub-components/scrubber';
 
 const Card = () => {
     return (
-        <View style={card_styles.card}>
+        <View style={[card_styles.card, card_styles.shadowCard]}>
             <ArtworkCover />
             <Text style={styles.text}>Come and Go</Text>
             <Text style={styles.sub_text}>Juice Wrld</Text>
@@ -19,11 +19,21 @@ const Card = () => {
 
 const card_styles = StyleSheet.create({
     card: {
-        marginTop: 30,
-        borderWidth: 3,
-        borderColor: "#cae3ea",
+        marginTop: 20,
+        backgroundColor: "#333",
         borderRadius: 15,
         padding: 30,
+    },
+    borderCard: {
+        borderWidth: 2,
+        borderColor: "#cae3ea",
+    },
+    shadowCard: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        elevation: 5
     }
 })
 
