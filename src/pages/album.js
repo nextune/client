9bbd772f81ebcbbbb13a05201d43b347cc7d27bf
Alex Chiommino <80229487+thedjchi@users.noreply.github.com>
2021-08-09 23:14:02 -0400

    refined sticky header

--- src/pages/album.js
+++ src/pages/album.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
-import { StatusBar, Animated, StyleSheet, SafeAreaView, View, useWindowDimensions, ImageBackground, Text } from 'react-native';
+import { StatusBar, Animated, StyleSheet, SafeAreaView, View, useWindowDimensions, ImageBackground, Text, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { FontAwesome5 } from '@expo/vector-icons';
 import Track from '../components/track';
 import Styles from '../styles';
 import { Colors } from '../colors';
@@ -41,42 +42,106 @@ const Album = () => {
         { title: "Juice WRLD Speaks From Heaven - Outro", artist: "Juice WRLD" },
     ]
 
-    const scrollY = useRef(new Animated.Value(0)).current;
+    const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
+    const scroll = useRef(new Animated.Value(0)).current;
+    const ref = useRef(null);
+
+    const stickyHeaderHeight = window.width * 0.64;
+
+    const getCloser = (value, checkOne, checkTwo) => Math.abs(value - checkOne) < Math.abs(value - checkTwo) ? checkOne : checkTwo;
 
     const handleScroll = Animated.event(
-        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-        { useNativeDriver: true },
+        [{ nativeEvent: { contentOffset: { y: scroll } } }],
+        { useNativeDriver: false }
     );
 
-    const headerTranslate = scrollY.interpolate({
-        inputRange: [0, window.width * 0.6],
-        outputRange: [0, -window.width * 0.6],
+    const handleSnap = ({ nativeEvent }) => {
+        const offset = nativeEvent.contentOffset.y;
+        const snapPoint = stickyHeaderHeight;
+        (offset > 0 && offset < snapPoint) ? ref.current ? ref.current.scrollToOffset({
+            offset: getCloser(offset, 0, stickyHeaderHeight) == 0 ? -snapPoint : snapPoint
+        }) : null : null;
+    }
+
+    const headerAnimation = (range) => (scroll.interpolate({
+        inputRange: [0, stickyHeaderHeight],
+        outputRange: range,
         extrapolate: 'clamp',
-    });
+    }))
+
+    const headerAnimationValues = {
+        collapse: [window.width, window.width - stickyHeaderHeight],
+        iconTranslate: [0, stickyHeaderHeight],
+        titleTranslate: [0, window.width * 0.02],
+        subtitleTranslate: [0, window.width * -0.05],
+        titleScale: [1, 0.75],
+        colorFadeIn: ['rgba(17,17,17,0)', 'rgba(17,17,17,0.67)'],
+        fadeIn: [0, 1],
+        fadeOut: [1, 0],
+    }
 
     const AlbumHeader = (props) => (
-        <Animated.View style={album_styles(headerTranslate).header}>
-            <ImageBackground source={props.art} style={album_styles().image_background}>
-                <LinearGradient colors={[Colors.TRANSLUCENT, Colors.TRANSLUCENTISH, 'transparent']} style={album_styles().statusbar_gradient} />
-                <LinearGradient colors={['transparent', Colors.TRANSLUCENTISH, Colors.DARK]} style={album_styles().gradient}>
-                    <Text numberOfLines={1} style={album_styles().header_title}>{props.title}</Text>
-                    <Text numberOfLines={1} style={album_styles().header_text}>{props.artist} ({props.year})</Text>
-                </LinearGradient>
-            </ImageBackground>
-        </Animated.View>
+        <View style={album_styles().header_container}>
+            <Animated.View style={[album_styles().header, { height: headerAnimation(headerAnimationValues.collapse) }]}>
+                <ImageBackground source={props.art} style={album_styles().image_background}>
+                    <Animated.View style={[album_styles().readability_overlay, {
+                        backgroundColor: headerAnimation(headerAnimationValues.colorFadeIn)
+                    }]}>
+                        <LinearGradient colors={[Colors.TRANSLUCENTISH, Colors.DARK]} style={album_styles().gradient}>
+                        <Animated.View style={[album_styles().icon_container, {
+                                    bottom: window.height * 0.325,
+                                    transform: [
+                                        { translateY: headerAnimation(headerAnimationValues.iconTranslate) }
+                                    ]
+                                }]}>
+                                <TouchableOpacity>
+                                    <FontAwesome5 name={"chevron-left"} color={Colors.WHITE} size={20} />
+                                </TouchableOpacity>
+                            </Animated.View>
+                            <View style={album_styles().text_container}>
+                                <Animated.Text numberOfLines={1} style={[album_styles().header_title, {
+                                    transform: [
+                                        { translateY: headerAnimation(headerAnimationValues.titleTranslate) },
+                                        { scale: headerAnimation(headerAnimationValues.titleScale) },
+                                    ]
+                                }]}>{props.title}</Animated.Text>
+                                <Animated.Text numberOfLines={1} style={[album_styles().header_subtitle, {
+                                    opacity: headerAnimation(headerAnimationValues.fadeOut),
+                                    transform: [
+                                        { translateY: headerAnimation(headerAnimationValues.subtitleTranslate) },
+                                    ]
+                                }]}>{props.artist} ({props.year})</Animated.Text>
+                            </View>
+                            <Animated.View style={[album_styles().icon_container, {
+                                    bottom: window.height * 0.325,
+                                    transform: [
+                                        { translateY: headerAnimation(headerAnimationValues.iconTranslate) }
+                                    ]
+                                }]}>
+                                <TouchableOpacity>
+                                    <FontAwesome5 name={"ellipsis-v"} color={Colors.WHITE} size={20} />
+                                </TouchableOpacity>
+                            </Animated.View>
+                        </LinearGradient>
+                    </Animated.View>
+                </ImageBackground>
+            </Animated.View>
+            <AnimatedLinearGradient colors={[Colors.DARK, Colors.TRANSLUCENT, 'transparent']} style={[album_styles().list_fade, { opacity: headerAnimation(headerAnimationValues.fadeIn) }]} />
+        </View>
     )
 
     return (
         <SafeAreaView style={Styles.body}>
-            <StatusBar barStyle="light" translucent={true} backgroundColor={Colors.TRANSLUCENT} />
+            <StatusBar barStyle="light" translucent={true} backgroundColor={'transparent'} />
             <AlbumHeader title={album.title} artist={album.artist} art={album.art} year={album.year} />
             <Animated.FlatList
-                style={album_styles().list}
-                contentContainerStyle={{paddingTop: window.width}}
+                contentContainerStyle={{ paddingTop: window.width }}
                 onScroll={handleScroll}
+                onMomentumScrollEnd={handleSnap}
+                fadingEdgeLength={100}
                 showsVerticalScrollIndicator={false}
                 overScrollMode="never"
-                fadingEdgeLength={100}
+                ref={ref}
                 data={tracks}
                 keyExtractor={(tracks, index) => index.toString()}
                 renderItem={({ item, index }) => (
@@ -98,44 +163,57 @@ const Album = () => {
     )
 }
 
-const album_styles = (headerTranslate) => {
+const album_styles = () => {
     const window = useWindowDimensions();
     return (
         StyleSheet.create({
-            header: {
+            header_container: {
                 position: 'absolute',
-                transform: [{ translateY: headerTranslate }],
-                aspectRatio: 1,
+                zIndex: 1,
+            },
+            header: {
                 width: window.width,
                 resizeMode: 'cover',
-                zIndex: 1,
+            },
+            readability_overlay: {
+                flex: 1,
+            },
+            icon_container: {
+                width: '5%',
+                alignItems: 'center',
+            },
+            text_container: {
+                flex: 1,
+            },
+            list_fade: {
+                height: window.width * 0.075,
             },
             image_background: {
                 flex: 1,
                 resizeMode: 'cover',
             },
             header_title: {
-                fontSize: 26,
+                fontSize: 24,
                 marginBottom: '-2%',
                 color: Colors.WHITE,
                 fontFamily: "Baloo2_700Bold",
                 textTransform: 'uppercase',
+                textAlign: 'center',
             },
-            header_text: {
-                fontSize: 14,
+            header_subtitle: {
+                fontSize: 12,
                 color: Colors.WHITE,
                 fontFamily: "Baloo2_500Medium",
                 textTransform: 'uppercase',
+                textAlign: 'center',
             },
             gradient: {
                 flex: 1,
+                flexDirection: 'row',
                 paddingHorizontal: '6%',
                 paddingBottom: '10%',
-                justifyContent: 'flex-end',
-                alignItems: 'center',
-            },
-            statusbar_gradient: {
-                height: '20%',
+                justifyContent: 'center',
+                alignItems: 'flex-end',
             },
             separator: {
                 backgroundColor: Colors.DARKISH,
